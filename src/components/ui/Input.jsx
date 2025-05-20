324e3fa787b4baa2b56b8307a10d20dd5aede6b9
Priyanshu Dayal <priyanshudayal1504@gmail.com>
2025-05-20 20:47:55 +0530

    feat(Input): replace SVG icons with react-icons for improved performance and maintainability

--- src/components/ui/Input.jsx
+++ src/components/ui/Input.jsx
@@ -1,4 +1,11 @@
 import React from "react";
+import {
+  MdEmail,
+  MdLock,
+  MdPerson,
+  MdPhone,
+  MdErrorOutline,
+} from "react-icons/md";
 
 const Input = ({
   label,
@@ -13,71 +20,17 @@ const Input = ({
 }) => {
   // Icon mapping function
   const getIcon = (iconName) => {
-    const iconClasses = "h-5 w-5";
     const iconStyle = { color: "var(--text-secondary)" };
 
     switch (iconName) {
       case "email":
-        return (
-          <svg
-            className={iconClasses}
-            style={iconStyle}
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
-            <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
-          </svg>
-        );
-
+        return <MdEmail style={iconStyle} className="h-5 w-5" />;
       case "lock":
-        return (
-          <svg
-            className={iconClasses}
-            style={iconStyle}
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path
-              fillRule="evenodd"
-              d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
-              clipRule="evenodd"
-            />
-          </svg>
-        );
-
+        return <MdLock style={iconStyle} className="h-5 w-5" />;
       case "user":
-        return (
-          <svg
-            className={iconClasses}
-            style={iconStyle}
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path
-              fillRule="evenodd"
-              d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
-              clipRule="evenodd"
-            />
-          </svg>
-        );
-
+        return <MdPerson style={iconStyle} className="h-5 w-5" />;
       case "phone":
-        return (
-          <svg
-            className={iconClasses}
-            style={iconStyle}
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
-          </svg>
-        );
-
+        return <MdPhone style={iconStyle} className="h-5 w-5" />;
       default:
         return null;
     }
@@ -166,18 +119,7 @@ const Input = ({
           className="mt-2 text-sm flex items-center animate-fadeIn"
           style={errorStyle}
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-4 w-4 mr-1.5"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path
-              fillRule="evenodd"
-              d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z"
-              clipRule="evenodd"
-            />
-          </svg>
+          <MdErrorOutline className="h-4 w-4 mr-1.5" />
           {error}
         </p>
       )}
